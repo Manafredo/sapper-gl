7dceb994579b590c5eca58734b97ea6bbf77b897
Manafredo <andrej.schulz@web.de>
2021-02-27 08:27:43 +0100

    first commit

---
+++ src/routes/impressum.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Impressum</title>
+</svelte:head>
+
+<h1>Impressum</h1>
+
+<p>There's not much here.</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,69 @@
+<script>
+	import { onMount } from "svelte";
+	import './button.scss';
+  import Button, {Group, GroupItem, Label, Icon} from '@smui/button';
+  import './button.scss';
+
+  let persons = [];
+
+  onMount(async function() {
+	const response = await fetch(
+	  "https://cdn.contentful.com/spaces/t170cpyn3oju/environments/master/entries/?content_type=person&include=0&select=sys.id,fields&access_token=MFnR8m8akJLpWiIGbewXZi_PgdWJ0lWv46tjhf7g4uU"
+	);
+	persons = await response.json();
+	persons =  persons.items;
+  });
+  let filter = "all";
+    const filterPersons = (filter, persons) =>
+      filter === "Politiker"
+        ? persons.filter(p => p.fields.category == "Politiker")
+        : filter === "Entrepreneur"
+        ? persons.filter(p => p.fields.category == "Entrepreneur")
+        : persons;
+</script>
+
+<style>
+	h1, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<div class="filterCategories btn-group stack-exception">
+    <Button class="btn toggle-btn" aria-pressed={filter === "Politiker"} on:click={()=> filter = "Politiker"} >
+      <span>Politiker</span>
+    </Button>
+    <Button class="btn toggle-btn"  aria-pressed={filter === "Entrepreneur"} on:click={()=> filter = "Entrepreneur"} >
+      <span>Entrepreneur</span>
+    </Button>
+    <Button class="btn toggle-btn" aria-pressed={filter === 'Alle'} on:click={()=> filter = 'Alle'} >
+      <span>Alle</span>
+    </Button>
+  </div>
+  <ul>
+    {#each filterPersons(filter,persons) as person}
+        <p>{person["fields"]["name"]}</p>
+        <p>{person["fields"]["description"]}</p>
+    {/each}
+  </ul>
